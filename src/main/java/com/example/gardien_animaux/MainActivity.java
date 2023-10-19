963d2df2d33cdac8a30cb9dca3d74228eb84c54f
yahya-daou <yayadou1006@gmail.com>
2023-10-19 17:30:41 +0200

    first commit

---
+++ src/main/java/com/example/gardien_animaux/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gardien_animaux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
